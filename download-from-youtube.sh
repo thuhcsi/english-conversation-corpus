4871d823431af7ebe6535d26f89dcf43356b7530
lijingbei <lijingbei@bytedance.com>
2021-12-20 19:34:18 +0800

    fixed filename for conversation 80

--- download-from-youtube.sh
+++ download-from-youtube.sh
@@ -11,6 +11,8 @@ do
 	)
 done
 
+mv 'data/English conversation 80-8_c0YgLOcdo.m4a' 'data/English Conversation 80-8_c0YgLOcdo.m4a'
+
 for i in data/*.webm
 do
 	ffmpeg -i $i -ac 1 -ar 16000 ${i%webm}wav
