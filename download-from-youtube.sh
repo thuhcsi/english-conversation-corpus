85f60f6f11ea7eb3eb1c02388cc994e1701dfe35
lijingbei <lijingbei@bytedance.com>
2022-02-04 16:56:48 +0800

    minor fixes

--- download-from-youtube.sh
+++ download-from-youtube.sh
@@ -15,10 +15,10 @@ mv 'data/English conversation 80-8_c0YgLOcdo.m4a' 'data/English Conversation 80-
 
 for i in data/*.webm
 do
-	ffmpeg -i $i -ac 1 -ar 16000 ${i%webm}wav
+	ffmpeg -i "$i" -ac 1 -ar 16000 "${i%webm}wav"
 done
 
 for i in data/*.m4a
 do
-	ffmpeg -i $i -ac 1 -ar 16000 ${i%m4a}wav
+	ffmpeg -i "$i" -ac 1 -ar 16000 "${i%m4a}wav"
 done
